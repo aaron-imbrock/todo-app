d21b7ebfdaceae1c63948efedd6caf18085e970d
Aaron Imbrock <aimbrock@gmail.com>
2024-05-27 22:08:27 -0700

    Hello World works

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "net/http"
+    "log"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello, World!"))
+    })
+
+    log.Println("Starting server on :8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
